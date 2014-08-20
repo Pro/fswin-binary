cf5d10fb920f2ea731fdc7b43a56fb5cb82f8ace
Steven Vachon <contact@svachon.com>
2014-08-20 15:11:42 -0400

    v0.1.1

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
+var fs = require("fs");
 var fsWin = null;
 var os = require("os").platform();
+var semver = require("semver");
+
+var extractedPath = "./vendor";
+var path = extractedPath;
 
 
 
-if (os == "win64")
+// Best to be synchronous for module.exports
+fs.readdirSync(path).every( function(file)
 {
-	fsWin = require("./vendor/x64/fswin.node");
-}
-else if (os == "win32")
+	if ( semver.satisfies(process.versions.node, file) )
+	{
+		path += "/" + file;
+		
+		switch (os)
+		{
+			case "win32": path+="/x86"; break;
+			case "win64": path+="/x64"; break;
+			default: path=null;
+		}
+		
+		return false;
+	}
+	
+	return true;
+});
+
+
+if (path && path!=extractedPath)
 {
-	fsWin = require("./vendor/x86/fswin.node");
+	fsWin = require(path+"/fswin.node");
 }
 
 
