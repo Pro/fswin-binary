10ccd455b171ff3326d98ecf8db51c52a76f15ae
Stefan Profanter <git@s.profanter.me>
2014-08-27 22:21:07 +0200

    Added check for windows platform

--- index.js
+++ index.js
@@ -9,29 +9,32 @@ var vendorPath = extractedPath;
 
 
 
-// Best to be synchronous for module.exports
-fs.readdirSync(vendorPath).every( function(file)
+if (process.platform.indexOf("win") == 0)
 {
-	if ( semver.satisfies(process.versions.node, file) )
+	// Best to be synchronous for module.exports
+	fs.readdirSync(vendorPath).every( function(file)
 	{
-		vendorPath = path.join(vendorPath,file);
-		switch (os)
+		if ( semver.satisfies(process.versions.node, file) )
 		{
-			case "x86": vendorPath=path.join(vendorPath,"x86"); break;
-			case "x64": vendorPath=path.join(vendorPath,"x64"); break;
-			default: vendorPath=null;
+			vendorPath = path.join(vendorPath,file);
+			switch (os)
+			{
+				case "x86": vendorPath=path.join(vendorPath,"x86"); break;
+				case "x64": vendorPath=path.join(vendorPath,"x64"); break;
+				default: vendorPath=null;
+			}
+			
+			return false;
 		}
 		
-		return false;
+		return true;
+	});
+
+
+	if (vendorPath && vendorPath!=extractedPath)
+	{
+		fsWin = require(path.join(vendorPath,"fswin.node"));
 	}
-	
-	return true;
-});
-
-
-if (vendorPath && vendorPath!=extractedPath)
-{
-	fsWin = require(path.join(vendorPath,"fswin.node"));
 }
 
 
